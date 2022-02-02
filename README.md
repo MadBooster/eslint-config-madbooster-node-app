2c6c85085021df5f30fd3fb17f77f35175432fef
Tomi Nokkala <tomi.nokkala@madbooster.com>
2022-02-02 13:30:58 +0200

    Optimize by extracting common configs to own package

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ### Usage
 * install package
-* install package and peer dependencies
-  * `npm install -D eslint-config-madbooster-node-app@latest eslint@">=7.14" eslint-config-standard@^16.0.2 eslint-plugin-import@^2.22.1 eslint-plugin-node@^11.1.0 eslint-plugin-promise@^5.1.0`
+  * `npm install -D eslint-config-madbooster-node-app@latest`
+* If not using npm => 8.0.0 : Install peer dependencies by hand
 * create .eslintrc file with:
   ```
   "extends": [
--- index.js
+++ index.js
@@ -1,93 +1,11 @@
 module.exports = {
-  plugins: [
-    'lodash',
-    'lodash-fp',
-    'you-dont-need-lodash-underscore'
-  ],
   extends: [
     'plugin:promise/recommended',
     'standard',
-    'plugin:lodash/recommended',
-    'plugin:lodash-fp/recommended',
-    'plugin:you-dont-need-lodash-underscore/compatible-warn'
+    'madbooster-common'
   ],
   rules: {
-    'arrow-parens': [1, 'as-needed'],
-    'generator-star-spacing': [
-      'error',
-      {
-        before: false,
-        after: true
-      }
-    ],
-    'keyword-spacing': [
-      'error',
-      {
-        after: true,
-        before: true,
-        overrides: {
-          if: {
-            after: false
-          },
-          for: {
-            after: false
-          },
-          while: {
-            after: false
-          },
-          switch: {
-            after: false
-          },
-          catch: {
-            after: false
-          }
-        }
-      }
-    ],
-    'no-console': [
-      'warn',
-      {
-        allow: ['warn', 'error']
-      }
-    ],
-    'no-unused-vars': [
-      'warn',
-      {
-        varsIgnorePattern: '^__',
-        argsIgnorePattern: '^__',
-        caughtErrorsIgnorePattern: '^__',
-        // from extended configs
-        args: 'none',
-        ignoreRestSiblings: true
-      }
-    ],
-    'object-curly-spacing': [
-      'error',
-      'always'
-    ],
-    quotes: [
-      'error',
-      'single',
-      {
-        allowTemplateLiterals: true
-      }
-    ],
-    'space-before-function-paren': [
-      'error',
-      'never'
-    ],
-
     'promise/no-callback-in-promise': 0, // in express you need to call next()
-    'promise/no-nesting': 0,
-
-    'lodash/import-scope': [1, 'method'],
-    'lodash/prefer-lodash-method': 0,
-
-    'lodash-fp/no-extraneous-args': 0,
-    'lodash-fp/prefer-get': 0,
-    'lodash-fp/use-fp': 0,
-
-    'you-dont-need-lodash-underscore/is-function': 0,
-    'you-dont-need-lodash-underscore/is-string': 0
+    'promise/no-nesting': 0
   }
 }
