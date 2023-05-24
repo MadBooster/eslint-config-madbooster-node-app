eb3a8dcb7f4ffc3ea25d2b65548373ca84779417
Tomi Nokkala <tomi.nokkala@madbooster.com>
2023-05-24 10:15:09 +0300

    Add some rules and move some to common

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ module.exports = {
   ],
   rules: {
     'promise/no-callback-in-promise': 0, // in express you need to call next()
-    'promise/no-nesting': 0
+    'promise/no-nesting': 0,
+    
+    'n/no-missing-import': 2,
+    'n/no-extraneous-import': 2,
+    'n/file-extension-in-import': 2,
+
+    'import/no-unresolved': 2,
+    'import/no-useless-path-segments': 2,
+    'import/no-commonjs': 2,
+    'import/extensions': [2, 'ignorePackages'],
+    'import/no-extraneous-dependencies': 2,
+    
+    'unicorn/prefer-module': 2,
+    'unicorn/prefer-node-protocol': 2
   }
 }
