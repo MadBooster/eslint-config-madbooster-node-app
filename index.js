f1d1eaaf0c7b1ba60b8b1fa411aae457a008b3ab
Tomi Nokkala <tomi.nokkala@madbooster.com>
2023-09-04 15:18:59 +0300

    Remove duplicated rules from common

--- index.js
+++ index.js
@@ -12,11 +12,8 @@ module.exports = {
     'n/no-extraneous-import': 2,
     'n/file-extension-in-import': 2,
 
-    'import/no-unresolved': 2,
-    'import/no-useless-path-segments': 2,
     'import/no-commonjs': 2,
     'import/extensions': [2, 'ignorePackages'],
-    'import/no-extraneous-dependencies': 2,
     
     'unicorn/prefer-module': 2,
     'unicorn/prefer-node-protocol': 2
