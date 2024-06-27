d988bc1bd668565d2053275659d96767f5f00fa5
Tomi Nokkala <tomi.nokkala@madbooster.com>
2024-06-27 16:31:04 +0300

    Add some typescript rules

--- index.js
+++ index.js
@@ -7,15 +7,48 @@ module.exports = {
   rules: {
     'promise/no-callback-in-promise': 0, // in express you need to call next()
     'promise/no-nesting': 0,
-    
+
     'n/no-missing-import': 2,
     'n/no-extraneous-import': 2,
     'n/file-extension-in-import': 2,
 
     'import/no-commonjs': 2,
-    'import/extensions': [2, 'ignorePackages'],
-    
+    'import/extensions': [
+      2,
+      'ignorePackages',
+      {
+        js: 'ignorePackages',
+        jsx: 'ignorePackages',
+        mis: 'ignorePackages',
+        cjs: 'ignorePackages',
+        ts: 'never',
+        tsx: 'never',
+        mts: 'never',
+        cts: 'never'
+      }
+    ],
+    'n/file-extension-in-import': 0, // handled by import/extensions
+
     'unicorn/prefer-module': 2,
     'unicorn/prefer-node-protocol': 2
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      }
+    },
+    n: {
+      tryExtensions: ['.js', '.ts'],
+      typescriptExtensionMap: [
+        ['', '.js'],
+        ['.js', '.js'],
+        ['.ts', '.js'],
+        ['.ts', '.ts'],
+        ['.cts', '.cjs'],
+        ['.mts', '.mjs'],
+        ['.tsx', '.jsx']
+      ]
+    }
   }
 }
