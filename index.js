30cc99d7cafbcbddc588fcec39c5013d4a376eba
Tomi Nokkala <tomi.nokkala@madbooster.com>
2025-04-01 10:23:11 +0300

    Tune rules

--- index.js
+++ index.js
@@ -7,33 +7,9 @@ export default defineConfig(
     plugins.promise.configs['flat/recommended'],
     madboosterCommon(true),
     {
-      settings: {
-        'import-x/resolver': {
-          typescript: {
-            alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-          },
-        },
-
-        n: {
-          tryExtensions: ['.js', '.ts', '.d.ts'],
-
-          typescriptExtensionMap: [
-            ['', '.js'],
-            ['.js', '.js'],
-            ['.ts', '.js'],
-            ['.ts', '.ts'],
-            ['.cts', '.cjs'],
-            ['.mts', '.mjs'],
-            ['.tsx', '.jsx'],
-          ],
-        },
-      },
-
       rules: {
         'promise/no-callback-in-promise': 0, // in express you need to call next()
         'promise/no-nesting': 0,
-        'n/no-missing-import': 2,
-        'n/no-extraneous-import': 2,
         'import-x/no-commonjs': 2,
 
         'import-x/extensions': [2, 'ignorePackages', {
